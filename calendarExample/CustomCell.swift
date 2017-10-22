31cbae3bf908e6085f5f1cd85e221dc99ba6a101
Anan Lappikulthong <ananlappikulthong@Anans-MacBook-Air.local>
2017-10-22 10:01:40 +0700

    4th

---
+++ calendarExample/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  calendarExample
+//
+//  Created by Anan Lappikulthong on 10/21/17.
+//  Copyright © 2017 Anan Lappikulthong. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel:UILabel!
+    @IBOutlet weak var selectedView:UIView!
+}
--- calendarExample/ViewController.swift
+++ calendarExample/ViewController.swift
@@ -7,12 +7,52 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
-
+    let formatter = DateFormatter()
+    
+    let outsideMonthColr = UIColor(colorWithHexValue: 0x584a66)
+    let monthColor = UIColor.white
+    let selectedMonthColor = UIColor(colorWithHexValue: 0x3a294b)
+    let currentDateSelectedViewColor = UIColor(colorWithHexValue: 0x4e3f5d)
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCalendarView()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupCalendarView() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState) {
+        guard let validcell = view as? CustomCell else { return }
+        
+        if cellState.isSelected {
+            validcell.dateLabel.textColor = selectedMonthColor
+        } else  {
+            if cellState.dateBelongsTo == .thisMonth {
+            validcell.dateLabel.textColor = monthColor
+            } else {
+                validcell.dateLabel.textColor = outsideMonthColr
+            }
+        }
+    }
+    
+    func handleCellSelected(view: JTAppleCell?, cellState: CellState) {
+        guard let validcell = view as? CustomCell else { return }
+        if validcell.isSelected {
+            validcell.selectedView.isHidden = false
+        } else {
+            validcell.selectedView.isHidden = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +61,56 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+}
+
+extension ViewController: JTAppleCalendarViewDelegate {
+    //Display cell
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        cell.dateLabel.text = cellState.text
+        
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellSelected(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+}
+
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha: CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            blue: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            alpha: alpha
+        )
+    }
 }
 
